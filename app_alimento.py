2b2b711ebbb8ea0b6770d5eb5d350308fdcf81a8
Cristian Tobar Morales <cristiantobar96@gmail.com>
2026-04-23 11:24:01 -0400

    Update app_alimento.py

--- app_alimento.py
+++ app_alimento.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from matplotlib.ticker import FuncFormatter
+from matplotlib.ticker import FuncFormatter 
 
 # ==============================================================================
 # 1. Configuración de la página
