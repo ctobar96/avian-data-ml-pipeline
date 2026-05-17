83d796119041b3760ac58054189d30d0803a47cc
Cristian Tobar <cristiantobar96@gmail.com>
2026-05-17 16:57:19 -0400

    Mejorar interfaz de usuario: eliminar encabezados de nivel en la sección de actualización de datos y filtro de visualización

--- app/app_alimento.py
+++ app/app_alimento.py
@@ -24,7 +24,7 @@ st.markdown("Monitorización del volumen de alimento fabricado y consumo de mate
 col_actualizar, col_selector = st.columns([1, 2])
 
 with col_actualizar:
-    st.subheader("### ⚡ Actualización de Datos")
+    st.subheader("⚡ Actualización de Datos")
     with st.expander("⬆️ Actualizar base de datos con nuevo Excel"):
         archivo_subido = st.file_uploader("Carga tu archivo de producción", type=["xls", "xlsx"])
         if archivo_subido is not None:
@@ -40,7 +40,7 @@ with col_actualizar:
                         st.error(f"Error de conexión: {e}")
 
 with col_selector:  
-    st.subheader("### 🔍 Filtro de Visualización")
+    st.subheader("🔍 Filtro de Visualización")
     # ==============================================================================
     # 3. SELECTORES DE FILTRO (MES Y SECTOR)
     # ==============================================================================
